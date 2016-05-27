ce07ba644ea138d59eb6223614d10fdc3d1382bd
Christophe Hurpeau <christophe@hurpeau.com>
2016-05-27 15:33:17 +0200

    acl.js : export default without const

--- src/core/acl.js
+++ src/core/acl.js
@@ -1,10 +1,8 @@
 import aclFactory from './library/acl-factory';
 import parameters from '../app/config/parameters';
 
-const acl = aclFactory({
+export default aclFactory({
   groups: parameters.acl.groups,
   profiles: parameters.acl.profiles,
   defaultGroup: parameters.acl.defaultGroup,
 });
-
-export default acl;
